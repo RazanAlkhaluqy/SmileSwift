fbcf50bc3bfb3b7c45d4fb1d8543fa329192b8b8
Razan aldakhil <aldrazu@gmail.com>
2024-03-25 02:36:25 +0300

    Reschedule page
    Reschedule page

---
+++ app/src/main/java/com/example/smileswift/reSchedule.java
@@ -0,0 +1,14 @@
+package com.example.smileswift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_re_schedule);
+    }
+}
