fc4d954626015828b2f60e79d9694a7472a12093
Razan aldakhil <aldrazu@gmail.com>
2024-03-25 04:45:44 +0300

    update

---
+++ app/src/main/java/com/example/smileswift/DeleteSchedule.java
@@ -0,0 +1,14 @@
+package com.example.smileswift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_schedule);
+    }
+}
