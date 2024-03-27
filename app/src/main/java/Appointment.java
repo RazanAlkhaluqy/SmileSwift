fa036f6b9262a7ccd14d3283d3844910d4884656
RazanAlkhaluqi <razan08642@gmail.com>
2024-03-27 22:59:27 +0300

    Create Appointment.java

---
+++ app/src/main/java/Appointment.java
@@ -0,0 +1,8 @@
+public class Appointment {
+    public static String Appointment_id;
+    public static String Appointment_Date;
+
+    public byte[] getApp() {
+        return new byte[0];
+    }
+}
