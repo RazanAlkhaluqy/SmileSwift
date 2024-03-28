508dc592e4b58229966ec03f8e22bb2015f526ff
RazanAlkhaluqi <razan08642@gmail.com>
2024-03-28 22:33:49 +0300

    Update DBsmileswift.java

--- app/src/main/java/com/example/smileswift/DBsmileswift.java
+++ app/src/main/java/com/example/smileswift/DBsmileswift.java
@@ -1,101 +1,112 @@
 package com.example.smileswift;
-
 import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
 public class DBsmileswift extends SQLiteOpenHelper {
 
-        public static final String DBNAME = "smileswift.db";
+    public static final String DBNAME = "smileswift.db";
 
-        public static int dbVersion=1;
-              //  Table1 Patient  //
-        public static final String T1_Patient = "Patient";
-        private static String idColumnP="Patient id";
-        public static final String COL1= "Patient name";
-        public static final String COL2 = "password";
-        public static final String numberP = "Patient Number";
+    // Table1 Patient
+    public static final String T1_Patient = "Patient";
+    public static final String COL1 = "Patient_name";
+    public static final String COL2 = "password";
+    public static final String numberP = "Patient_Number";
 
-               // Table2 Appointment //
-        private static String T2_Appointment= "Appointment";
-        private static String idColumnA="Appointment_id";
-        private static String dateColumnA="Appointment_Date";
-        private static String stateColumnA="Appointment_State";
+    // Table2 Appointment
+    public static final String T2_Appointment = "Appointment";
+    public static final String idColumnA = "Appointment_id";
+    public static final String dateColumnA = "Appointment_Date";
+    public static final String stateColumnA = "Appointment_State";
+    public static final String idColumnD = "Doctor_id";
+    public static final String idColumnP = "Patient_id";
 
-             // Table3 Doctor //
-        private static String T3_Doctor= "Doctor";
-        private static String idColumnD="Doctor id";
-        private static String nameColumnD="Doctor name";
-        private static String yearColumnD="Years Of Service";
+    // Table3 Doctor
+    public static final String T3_Doctor = "Doctor";
+    public static final String idColumnD_Doc = "Doctor_id";
+    public static final String nameColumnD = "Doctor_name";
+    public static final String yearColumnD = "Years_Of_Service";
 
+    public DBsmileswift( Context context) {
+        super(context, DBNAME, null, 1);
+    }
 
-        public DBsmileswift(@Nullable Context context) {
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + T1_Patient + "(" +
+                idColumnP + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL1 + " TEXT, " +
+                COL2 + " TEXT, " +
+                numberP + " TEXT)");
 
-            super(context, DBNAME, null, 1);
-        }
+        sqLiteDatabase.execSQL("CREATE TABLE " + T2_Appointment + "(" +
+                idColumnA + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                dateColumnA + " TEXT, " +
+                stateColumnA + " TEXT, " +
+                idColumnD + " INTEGER, " +
+                idColumnP + " INTEGER, " +
+                "FOREIGN KEY(" + idColumnD + ") REFERENCES " + T3_Doctor + "(" + idColumnD_Doc + "), " +
+                "FOREIGN KEY(" + idColumnP + ") REFERENCES " + T1_Patient + "(" + idColumnP + "))");
 
-        @Override
-        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + T3_Doctor + "(" +
+                idColumnD_Doc + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                nameColumnD + " TEXT, " +
+                yearColumnD + " INTEGER)");
+    }
 
-            sqLiteDatabase.execSQL("CREATE TABLE " + T1_Patient +"(" +
-                    idColumnP + " ,INTEGER PRIMARY KEY, " + // PK
-                     COL1 + ", TEXT , " + COL2 + " TEXT" +
-                    ")");
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + T1_Patient);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + T2_Appointment);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + T3_Doctor);
+        onCreate(sqLiteDatabase);
+    }
 
-            sqLiteDatabase.execSQL("CREATE TABLE " + T2_Appointment +
-                    "(" + idColumnA + " ,INTEGER PRIMARY KEY autoincrement, " +  // PK
-                    dateColumnA + "String ," +
-                    stateColumnA + "String ," +
-                    idColumnD +", INTEGER references"+T3_Doctor +idColumnD  + // FK
-                    idColumnP +", INTEGER references" +T1_Patient +"(" + idColumnP + ")" ); // FK
+    public boolean insertPatientData(String username, String password, String number) {
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL1, username);
+        contentValues.put(COL2, password);
+        contentValues.put(numberP, number);
+        long result = MyDB.insert(T1_Patient, null, contentValues);
+        return result != -1;
+    }
 
-            sqLiteDatabase.execSQL("CREATE TABLE " + T3_Doctor +
-                    "(" + idColumnD + " ,INTEGER PRIMARY KEY autoincrement, " + // PK
-                    nameColumnD + " ,TEXT ," +
-                    yearColumnD +" INTEGER " +
-                    ")");
-        }
+    public boolean insertAppointmentData(String date, String state, int doctorId, int patientId) {
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(dateColumnA, date);
+        contentValues.put(stateColumnA, state);
+        contentValues.put(idColumnD, doctorId);
+        contentValues.put(idColumnP, patientId);
+        long result = MyDB.insert(T2_Appointment, null, contentValues);
+        return result != -1;
+    }
 
-        @Override
-        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + T1_Patient);
-            onCreate(sqLiteDatabase);
-        }
+    public boolean insertDoctorData(String name, int yearsOfService) {
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(nameColumnD, name);
+        contentValues.put(yearColumnD, yearsOfService);
+        long result = MyDB.insert(T3_Doctor, null, contentValues);
+        return result != -1;
+    }
 
-        public Boolean insertData(String username, String password) {
-            SQLiteDatabase MyDB = this.getWritableDatabase();
-            ContentValues contentValues = new ContentValues();
-            contentValues.put(COL1, username);
-            contentValues.put(COL2, password);
-            long result = MyDB.insert(T1_Patient, null, contentValues);
-            return result != -1;
-        }
+    public boolean checkUsername(String username) {
+        SQLiteDatabase MyDB = this.getReadableDatabase(); // Change to getReadableDatabase()
+        Cursor cursor = MyDB.rawQuery("SELECT * FROM " + T1_Patient + " WHERE " + COL1 + " = ?", new String[]{username});
+        boolean exists = cursor.getCount() > 0;
+        cursor.close();
+        return exists;
+    }
 
-        public Boolean checkUsername(String username) {
-            SQLiteDatabase MyDB = this.getWritableDatabase();
-            Cursor cursor = MyDB.rawQuery("SELECT * FROM " + T1_Patient + " WHERE " + COL1 + " = ?", new String[]{username});
-            return cursor.getCount() > 0;
-        }
-
-        public Boolean checkUsernamePassword(String username, String password) {
-            SQLiteDatabase MyDB = this.getWritableDatabase();
-            Cursor cursor = MyDB.rawQuery("SELECT * FROM " + T1_Patient + " WHERE " + COL1 + " = ? AND " + COL2 + " = ?", new String[]{username, password});
-            return cursor.getCount() >0 ;
-        }
-   // New Methods :
-        public <Appointment> long Appointment(Appointment appointment) {
-           try {
-               SQLiteDatabase sqLiteDatabase= getWritableDatabase();
-               ContentValues contentValues= new ContentValues();
-               contentValues.put(dateColumnA, appointment.getClass().getModifiers());
-              return sqLiteDatabase.insert(T2_Appointment, null, contentValues) ;
-           }  finally {
-
-           }
+    public boolean checkUsernamePassword(String username, String password) {
+        SQLiteDatabase MyDB = this.getReadableDatabase(); // Change to getReadableDatabase()
+        Cursor cursor = MyDB.rawQuery("SELECT * FROM " + T1_Patient + " WHERE " + COL1 + " = ? AND " + COL2 + " = ?", new String[]{username, password});
+        boolean exists = cursor.getCount() > 0;
+        cursor.close();
+        return exists;
     }
 
 }
