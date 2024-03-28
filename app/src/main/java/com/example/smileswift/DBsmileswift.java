fa9a31e2b9db7c5df62f7b14598e1e8762316878
RazanAlkhaluqi <razan08642@gmail.com>
2024-03-28 22:41:09 +0300

    Update DBsmileswift.java

--- app/src/main/java/com/example/smileswift/DBsmileswift.java
+++ app/src/main/java/com/example/smileswift/DBsmileswift.java
@@ -1,6 +1,7 @@
 package com.example.smileswift;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
