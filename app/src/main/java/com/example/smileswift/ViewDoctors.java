7deac981224aafb2d0c7bed4379c3162aa29ce85
RazanAlkhaluqi <razan08642@gmail.com>
2024-03-28 19:42:58 +0300

    Create ViewDoctors.java

---
+++ app/src/main/java/com/example/smileswift/ViewDoctors.java
@@ -0,0 +1,14 @@
+package com.example.smileswift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewDoctors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_doctors);
+    }
+}
