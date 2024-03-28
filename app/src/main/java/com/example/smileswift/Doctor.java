2314750a65caf94b05d448a73bd7cad6f93079f3
RazanAlkhaluqi <razan08642@gmail.com>
2024-03-28 22:33:53 +0300

    Update Doctor.java

--- app/src/main/java/com/example/smileswift/Doctor.java
+++ app/src/main/java/com/example/smileswift/Doctor.java
@@ -1,4 +1,39 @@
 package com.example.smileswift;
-
 public class Doctor {
+    private int id;
+    private String name;
+    private int yearsOfService;
+
+    // Constructor
+    public Doctor(int id, String name, int yearsOfService) {
+        this.id = id;
+        this.name = name;
+        this.yearsOfService = yearsOfService;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearsOfService() {
+        return yearsOfService;
+    }
+
+    public void setYearsOfService(int yearsOfService) {
+        this.yearsOfService = yearsOfService;
+    }
 }
+
