e30134c780b092cdec02b4757e95a8943cba92f3
RazanAlkhaluqi <razan08642@gmail.com>
2024-03-28 22:33:52 +0300

    Update Patient.java

--- app/src/main/java/com/example/smileswift/Patient.java
+++ app/src/main/java/com/example/smileswift/Patient.java
@@ -1,4 +1,49 @@
 package com.example.smileswift;
-
 public class Patient {
+    private int id;
+    private String name;
+    private String password;
+    private String number;
+
+    // Constructor
+    public Patient(int id, String name, String password, String number) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.number = number;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
+
