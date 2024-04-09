a8b9c59a253bb631b99db703806947b3a7e77655
Razan aldakhil <aldrazu@gmail.com>
2024-04-09 08:57:07 +0300

    Update DeleteSchedule.java

--- app/src/main/java/com/example/smileswift/DeleteSchedule.java
+++ app/src/main/java/com/example/smileswift/DeleteSchedule.java
@@ -1,8 +1,12 @@
 package com.example.smileswift;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DeleteSchedule extends AppCompatActivity {
 
@@ -10,5 +14,49 @@ public class DeleteSchedule extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_schedule);
+
+        // Initialize your views
+
+        // Set up the cancel button
+        Button cancelButton = findViewById(R.id.cancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        // Set up the delete button
+        Button deleteButton = findViewById(R.id.Delete);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDeleteAppointmentDialog();
+            }
+        });
+    }
+
+    private void showDeleteAppointmentDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete Appointment");
+        builder.setView(R.layout.dialog_delete_appointment);
+
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Handle the delete action here
+                // You can use the DBsmileswift class to delete the appointment
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
